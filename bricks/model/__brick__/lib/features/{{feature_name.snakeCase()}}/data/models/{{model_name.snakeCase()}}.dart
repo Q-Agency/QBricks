7d03e2f065e60f4a0d662b7d68a6d5b7aab4b178
dmesek <dominik.mesek@q.agency>
2022-11-14 09:45:10 +0100

    delegating generation to jsonSerializable for model brick

--- bricks/model/__brick__/lib/features/{{feature_name.snakeCase()}}/data/models/{{model_name.snakeCase()}}.dart
+++ bricks/model/__brick__/lib/features/{{feature_name.snakeCase()}}/data/models/{{model_name.snakeCase()}}.dart
@@ -1,30 +1,15 @@
-import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 part '{{model_name.snakeCase()}}.g.dart';
 
-class {{model_name.pascalCase()}} extends Equatable {
+@JsonSerializable()
+class {{model_name.pascalCase()}} {
   {{#properties}}
-
   final {{#hasSpecial}}{{{type}}}{{/hasSpecial}}{{^hasSpecial}}{{type}}{{/hasSpecial}} {{name}};{{/properties}}
+  
   const {{model_name.pascalCase()}}({{#hasProperties}}{ {{#properties}}
     required this.{{name}},{{/properties}}
   }{{/hasProperties}});
 
-  factory {{model_name.pascalCase()}}.fromJson(Map<String, dynamic> data) => _${{model_name.pascalCase()}}FromJson(data);
-
-
-  {{model_name.pascalCase()}} copyWith({{#hasProperties}}{ {{#properties}}
-    {{#hasSpecial}}{{{type}}}{{/hasSpecial}}{{^hasSpecial}}{{type}}{{/hasSpecial}}? {{name}},{{/properties}}
-  }{{/hasProperties}}) {
-    return {{model_name.pascalCase()}}({{#properties}}
-      {{name}}: {{name}} ?? this.{{name}},{{/properties}}
-    );
-  }
-
-  @override
-  List<Object?> get props => [{{#properties}}
-        {{name}},{{/properties}}
-      ];
-
-  Map<String, dynamic> toJson() => _${{model_name.pascalCase()}}ToJson(this);
+  factory {{model_name.pascalCase()}}.fromJson(Map<String, dynamic> json) => _${{model_name.pascalCase()}}FromJson(json);
 }
