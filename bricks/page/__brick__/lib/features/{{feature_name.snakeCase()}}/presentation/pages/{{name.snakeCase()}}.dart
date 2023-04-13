b843280a079cfc40b373b653406204764c90dc6a
Ivan Celija <ivan.celija@q.agency>
2023-04-13 09:33:58 +0200

    Added dependencies into Page brick template

--- bricks/page/__brick__/lib/features/{{feature_name.snakeCase()}}/presentation/pages/{{name.snakeCase()}}.dart
+++ bricks/page/__brick__/lib/features/{{feature_name.snakeCase()}}/presentation/pages/{{name.snakeCase()}}.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+{{#dependencies}}
+import 'package:{{project_name.snakeCase()}}/features/{{feature_name.snakeCase()}}/domain/notifiers/{{dependencyName.snakeCase()}}.dart';
+{{/dependencies}}
 
 class {{name.pascalCase()}} extends ConsumerWidget {
   const {{name.pascalCase()}}({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    {{#dependencies}}final {{dependencyName.camelCase()}}State = ref.watch({{dependencyName.camelCase()}}Provider);
+    {{/dependencies}}
     return const Scaffold(
       body: Placeholder(),
     );
