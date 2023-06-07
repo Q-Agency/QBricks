9e68474a49eca86137be8df4da7ad086f682cd36
Ivan Celija <ivan.celija@q.agency>
2023-06-07 13:44:33 +0200

    Added system_foreground_notifications flag to show system push notifications in foreground on iOS and Android (flutter_local_notifications)

--- bricks/firebase_messaging/__brick__/lib/features/firebase_messaging/data/entities/firebase_messaging_notification.dart
+++ bricks/firebase_messaging/__brick__/lib/features/firebase_messaging/data/entities/firebase_messaging_notification.dart
@@ -1,14 +1,52 @@
 import 'package:equatable/equatable.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:{{project_name.snakeCase()}}/features/firebase_messaging/data/repositories/firebase_messaging_repository.dart';
 
+part 'firebase_messaging_notification.g.dart';
+
+@JsonSerializable()
 class FirebaseMessagingNotification extends Equatable {
+  @_RemoteMessageConverter()
   final RemoteMessage remoteMessage;
   final NotificationStartedType notificationStartedType;
 
-  const FirebaseMessagingNotification({required this.remoteMessage, required this.notificationStartedType});
+  const FirebaseMessagingNotification({
+    required this.remoteMessage,
+    required this.notificationStartedType,
+  });
+
+  factory FirebaseMessagingNotification.fromJson(Map<String, dynamic> json) =>
+      _$FirebaseMessagingNotificationFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FirebaseMessagingNotificationToJson(this);
+
+  FirebaseMessagingNotification copyWith({
+    RemoteMessage? remoteMessage,
+    NotificationStartedType? notificationStartedType,
+  }) {
+    return FirebaseMessagingNotification(
+      remoteMessage: remoteMessage ?? this.remoteMessage,
+      notificationStartedType:
+          notificationStartedType ?? this.notificationStartedType,
+    );
+  }
 
   @override
   List<Object?> get props => [remoteMessage, notificationStartedType];
+}
+
+class _RemoteMessageConverter
+    extends JsonConverter<RemoteMessage, Map<String, dynamic>> {
+  const _RemoteMessageConverter();
+
+  @override
+  RemoteMessage fromJson(json) {
+    return RemoteMessage.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic> toJson(object) {
+    return object.toMap();
+  }
 }
