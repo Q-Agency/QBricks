15c71910dc007313e87f6dbad20408b95a5bcdc1
Ivan Celija <ivan.celija@q.agency>
2023-12-07 16:22:05 +0100

    Added optional is not required in entity and model constructors

--- bricks/model/__brick__/lib/features/{{feature_name.snakeCase()}}/data/models/{{model_name.snakeCase()}}.dart
+++ bricks/model/__brick__/lib/features/{{feature_name.snakeCase()}}/data/models/{{model_name.snakeCase()}}.dart
@@ -8,7 +8,7 @@ class {{model_name.pascalCase()}} {
   final {{#hasSpecial}}{{{type}}}{{/hasSpecial}}{{^hasSpecial}}{{type}}{{/hasSpecial}} {{name}};{{/properties}}
   
   const {{model_name.pascalCase()}}({{#hasProperties}}{ {{#properties}}
-    required this.{{name}},{{/properties}}
+    {{^isOptional}}required{{/isOptional}} this.{{name}},{{/properties}}
   }{{/hasProperties}});
 
   factory {{model_name.pascalCase()}}.fromJson(Map<String, dynamic> json) => _${{model_name.pascalCase()}}FromJson(json);
